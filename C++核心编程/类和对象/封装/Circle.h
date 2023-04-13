5a1c5289f4a702ebcbbb816a4ee7f5d40cca95a7
Chen <129747426+ChenRD98@users.noreply.github.com>
2023-04-13 12:48:51 +0800

    案例2点和圆的关系

---
+++ C++核心编程/类和对象/封装/Circle.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "Point.h"
+class Circle {
+public:
+	void setR(int r);
+	int getR();
+	void setCenter(Point center);
+	Point getCenter();
+private:
+	int m_R;
+	Point m_Center;
+};
---
+++ C++核心编程/类和对象/封装/Point.h
@@ -0,0 +1,11 @@
+#pragma once
+class Point {
+public:
+	void setX(int x);
+	int getX();
+	void setY(int y);
+	int getY();
+private:
+	int m_X;
+	int m_Y;
+};
