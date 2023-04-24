27ba10abe3b8d0973400d800cb5324a69ac64429
Chen <129747426+ChenRD98@users.noreply.github.com>
2023-04-24 16:19:08 +0800

    修改职工

--- C++核心编程/职工管理系统/workerManager.h
+++ C++核心编程/职工管理系统/workerManager.h
@@ -32,7 +32,8 @@ public:
 	void Del_Emp();
 	//按照职工编号判断职工是否存在，若存在返回职工在数组中的位置，不存在返回-1
 	int IsExist(int id);
-
+	//修改职工
+	void Mod_Emp();
 
 
 
--- C++核心编程/职工管理系统/职工管理系统.cpp
+++ C++核心编程/职工管理系统/职工管理系统.cpp
@@ -40,12 +40,15 @@ int main() {
 			break;
 		case 2:
 			//显示职工
+			wm.Show_Emp();
 			break;
 		case 3:
 			//删除职工
+			wm.Del_Emp();
 			break;
 		case 4:
 			//修改职工
+			wm.Mod_Emp();
 			break;
 		case 5:
 			//查找职工
