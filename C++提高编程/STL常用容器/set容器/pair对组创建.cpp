21d466a0cb1ef451e02bd05e2db0354a197199a4
Chen <129747426+ChenRD98@users.noreply.github.com>
2023-05-19 14:21:09 +0800

    pair对组创建.cpp

---
+++ C++提高编程/STL常用容器/set容器/pair对组创建.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+void test01() {
+	pair<string, int> p(string("Tom"), 20);
+	cout << "姓名：" << p.first << " 年龄：" << p.second << endl;
+	pair<string, int> p2 = make_pair(string("Jerry"), 10);
+	cout << "姓名：" << p2.first << " 年龄：" << p2.second << endl;
+}
+
+int main() {
+	test01();
+	system("pause");
+	return 0;
+}
