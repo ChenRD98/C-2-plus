825857b2ffdbe320bac1994207b5d1f49187cee0
Chen <129747426+ChenRD98@users.noreply.github.com>
2023-05-28 10:32:12 +0800

    sort.cpp

---
+++ C++提高编程/STL常用算法/常用排序算法/sort.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<functional>
+using namespace std;
+
+class Print01 {
+public:
+	void operator()(int val) {
+		cout << val << " ";
+	}
+};
+
+void test01() {
+	vector<int>v;
+	v.push_back(10);
+	v.push_back(30);
+	v.push_back(50);
+	v.push_back(20);
+	v.push_back(40);
+	for_each(v.begin(), v.end(), Print01());
+	cout << endl;
+
+	sort(v.begin(), v.end());
+	for_each(v.begin(), v.end(), Print01());
+	cout << endl;
+	
+	sort(v.begin(), v.end(), greater<int>());
+	for_each(v.begin(), v.end(), Print01());
+	cout << endl;
+}
+
+int main() {
+	test01();
+	system("pause");
+	return 0;
+}
