b126c2bf1bfbeee98ef90c423a48c447d8d81dc6
Chen <129747426+ChenRD98@users.noreply.github.com>
2023-06-08 16:11:21 +0800

    菜单实现

---
+++ C++提高编程/机房预约系统/机房预约系统.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+
+int main() {
+	cout << "-----------------------  欢迎使用机房预约系统  -----------------------" << endl;
+	cout << "\t\t ---------------------------------- \n";
+	cout << "\t\t|                                  |\n";
+	cout << "\t\t|            1.学生代表            |\n";
+	cout << "\t\t|                                  |\n";
+	cout << "\t\t|            2.老    师            |\n";
+	cout << "\t\t|                                  |\n";
+	cout << "\t\t|            1.管 理 员            |\n";
+	cout << "\t\t|                                  |\n";
+	cout << "\t\t|            2.退    出            |\n";
+	cout << "\t\t|                                  |\n";
+	cout << "\t\t ---------------------------------- \n";
+	cout << "请输入您的选择：\n";
+
+
+
+
+	system("pause");
+	return 0;
+}
